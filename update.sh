341a3c66884bd87cdefe0bacb2df5630441af8b6
wrtpi <71971470+wrtpi@users.noreply.github.com>
2024-01-28 23:41:34 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -50,9 +50,9 @@ apk update && apk upgrade
 echo "----------------------------------------------------------------------"
 sleep 2
 echo "安装常用工具"
-apk add  curl wget unzip htop screen ffmpeg net-tools iputils-ping bash-completion vim neovim nano neofetch jq
-#⭐️#apt install curl wget unzip htop screen ffmpeg net-tools iputils-ping bash-completion vim neovim nano neofetch jq
-#apt install curl wget unzip htop screen ffmpeg net-tools iputils-ping bash-completion vim neovim nano neofetch jq -qq
+apk add sudo curl wget unzip htop screen ffmpeg net-tools iputils-ping bash-completion vim neovim nano neofetch jq
+#⭐️#apt install sudo curl wget unzip htop screen ffmpeg net-tools iputils-ping bash-completion vim neovim nano neofetch jq
+#apt install sudo curl wget unzip htop screen ffmpeg net-tools iputils-ping bash-completion vim neovim nano neofetch jq -qq
 echo "----------------------------------------------------------------------"
 #⭐️#
 echo "安装命令工具"
